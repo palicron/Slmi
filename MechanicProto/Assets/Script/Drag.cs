96e0a27af29a229c414df2cf6a1f65f528638d39
desktop-palicron <elgranpalicron@gmail.com>
2018-06-24 11:23:59 -0500

    inicial

---
+++ MechanicProto/Assets/Script/Drag.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour {
+
+    Vector3 dist;
+    float posX;
+    float posY;
+	Vector3 curPos;
+	Rigidbody rb;
+  void OnMouseDown(){
+      dist = Camera.main.WorldToScreenPoint(transform.position);
+      posX = Input.mousePosition.x - dist.x;
+      posY = Input.mousePosition.y - dist.y;
+		rb = this.GetComponent<Rigidbody>();
+
+    }
+
+  void OnMouseDrag(){
+       curPos = 
+                new Vector3(Input.mousePosition.x - posX, 
+                Input.mousePosition.y - posY, dist.z);
+	
+		curPos.z = 0;
+	  Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+		worldPos.z = 0;
+		
+		
+
+		if (Vector3.Distance(worldPos, transform.position) < 1f)
+		{
+			rb.AddForce((worldPos - transform.position).normalized*0.7f , ForceMode.Impulse);
+		}
+		else
+		{
+			rb.AddForce((worldPos - transform.position).normalized * 5f, ForceMode.Impulse);
+		}
+	
+			
+      //transform.position = worldPos;
+  }
+
+	private void OnMouseUp()
+	{
+		rb.isKinematic = false;
+		rb.useGravity = true;
+	}
+}
---
+++ MechanicProto/Assets/Script/Player_Ctr.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Ctr : MonoBehaviour {
+
+	public float speed;
+	public float forceJump;
+	public float detecDistance;
+	public ForceMode force;
+	private Rigidbody rb;
+	[SerializeField]
+	private bool onGround;
+	
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		onGround = ground();
+		moveforwrd();
+		if (!onGround)
+			return;
+	
+		bool hit = Physics.Raycast(transform.position, transform.forward, detecDistance);
+		if (hit)
+			rb.AddForce(transform.up * forceJump, force);
+	}
+
+	private void moveforwrd()
+	{
+		transform.position += transform.forward * speed * Time.deltaTime;
+
+	}
+	private bool ground()
+	{
+		return Physics.Raycast(transform.position, -transform.up, 0.5f);
+	}
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawRay(transform.position, transform.forward* detecDistance);
+		Gizmos.DrawRay(transform.position, -transform.up * 0.5f);
+	}
+
+}
